0cc470e2257796d628e2c3f3ea0bbc2a68944f22
rupesh4950 <101011309+rupesh4950@users.noreply.github.com>
2023-01-07 17:05:36 +0530

    Add files via upload

--- thilak1.java
+++ thilak1.java
@@ -1,19 +1,15 @@
-package constructor;
-
+package TEsts;
 public class thilak1 {
     public static void main(String[] args) {
         String s="welcomr to world";
         String a[]=s.split(" ");
         String temp=a[0];
-        char t[]={'a','b','c'};
         a[0]=a[a.length-1];
         a[a.length-1]=temp;
         String fin="";
-        String m=new String(t);
         for (int i=0;i<a.length;i++){
-            fin+=a[i]+" ";
+            fin+=a[i];
         }
         System.out.println(fin);
-        System.err.println(m);
     }
 }
